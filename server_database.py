66bdf99839c6285cb6fbf9feb83e2f562ff8a126
Juan Miguel Montemayor <jmmontem@uci.edu>
2019-08-17 03:02:33 -0700

    Added ability for database to add information

--- server_database.py
+++ server_database.py
@@ -1,5 +1,7 @@
 import json
 
+DB_Path = '.\DB'
+
 class database_manager:
 
     def __init__(self):
@@ -17,11 +19,55 @@ class database_manager:
 
     # Code for updating the database
 
-    def add_zipcode(self):
-        pass
+    def add_zipcode(self, zip_code: str) -> None:
+        ''' Add Zipcode in the json Database '''
+        file_path = f".\DB\{zip_code}.json"
+        try:
+            with open(file_path, "r") as json_file:
+                print("Already exist!")
 
-    def add_event(self):
-        pass
+        except FileNotFoundError:
+            self._dump_data(file_path, {})
+            
+    def add_event(self, zip_code: str, event: str) -> None:
+        ''' Add Event in the json Database '''
+        file_path = f".\DB\{zip_code}.json"
+        json_object = dict()
 
-    def add_message(self):
-        pass
+        self._open_data(file_path, json_object)
+
+        json_object[event] = []
+
+        self._dump_data(file_path, json_object)
+
+        
+    def add_message(self, zip_code: str, event: str, message: str) -> None:
+        ''' Add Message in the json Database '''
+        file_path = f".\DB\{zip_code}.json"
+        json_object = dict()
+        
+        self._open_data(file_path, json_object)
+
+        json_object[event].append(message)
+
+        self._dump_data(file_path, json_object)
+
+
+    def _open_data(self, file_path: str, json_dict: dict) -> None:
+        with open(file_path, "r") as json_file:
+            json_dict = json.load(json_file)
+        
+
+
+    def _dump_data(self, file_path: str, data: dict) -> None:
+        ''' Dump data to json file '''
+        with open(file_path, "w") as json_file:
+            json.dump(data, json_file)
+
+
+
+
+x = database_manager()
+x.add_zipcode("94533")
+x.add_event("94533", "Fight")
+x.add_message("94533", "Fight", "Hello")
